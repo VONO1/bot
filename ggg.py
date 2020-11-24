bb25cd2a8d464e95d092d55b90ab90541bf4b7ff
komarov.i <mr.vono@gmail.com>
2020-11-24 18:25:47 +0300

    444

--- ggg.py
+++ ggg.py
@@ -1,8 +1,52 @@
-gl =10
+# Подключаем модуль случайных чисел
+import random
+# Подключаем модуль для Телеграма
+import telebot
+# Указываем токен
+bot = telebot.TeleBot('1457884681:AAF9GjlOLP7hlhlqimmIMm5heybKXv-Ig28')
+# Импортируем типы из модуля, чтобы создавать кнопки
+from telebot import types
 
-def AS():
-    global gl
-    gl = gl + 10
 
-AS()
-print(gl)
+bot.polling(none_stop=True, interval=0)
+
+
+name = '';
+surname = '';
+age = 0;
+@bot.message_handler(content_types=['text'])
+def start(message):
+    if message.text == '/reg':
+        bot.send_message(message.from_user.id, "Как тебя зовут?");
+        bot.register_next_step_handler(message, get_name); #следующий шаг – функция get_name
+    else:
+        bot.send_message(message.from_user.id, 'Напиши /reg');
+
+def get_name(message): #получаем фамилию
+    global name;
+    name = message.text;
+    bot.send_message(message.from_user.id, 'Какая у тебя фамилия?');
+    bot.register_next_step_handler(message, get_surnme);
+
+def get_surname(message):
+    global surname;
+    surname = message.text;
+    bot.send_message('Сколько тебе лет?');
+    bot.register_next_step_handler(message, get_age);
+
+def get_age(message):
+    keyboard = types.InlineKeyboardMarkup(); #наша клавиатура
+    key_yes = types.InlineKeyboardButton(text='Да', callback_data='yes'); #кнопка «Да»
+    keyboard.add(key_yes); #добавляем кнопку в клавиатуру
+    key_no= types.InlineKeyboardButton(text='Нет', callback_data='no');
+    keyboard.add(key_no);
+    question = 'Тебе '+str(age)+' лет, тебя зовут '+name+' '+surname+'?';
+    bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    if call.data == "yes": #call.data это callback_data, которую мы указали при объявлении кнопки
+        #код сохранения данных, или их обработки
+        bot.send_message(call.message.chat.id, 'Запомню : )');
+    elif call.data == "no":
+        pass
+         #переспрашиваем
