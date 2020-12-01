2c9118cf52900e26881235f790ca79397916421a
komarov.i <mr.vono@gmail.com>
2020-12-01 16:25:43 +0300

    444

--- ggg.py
+++ ggg.py
@@ -2,51 +2,63 @@
 import random
 # Подключаем модуль для Телеграма
 import telebot
+
 # Указываем токен
-bot = telebot.TeleBot('1457884681:AAF9GjlOLP7hlhlqimmIMm5heybKXv-Ig28')
+client = telebot.TeleBot('1457884681:AAF9GjlOLP7hlhlqimmIMm5heybKXv-Ig28')
+
+client.enable_save_next_step_handlers(delay=2)
 # Импортируем типы из модуля, чтобы создавать кнопки
 from telebot import types
 
+markup_inline = types.InlineKeyboardMarkup()
+item_yes = types.InlineKeyboardButton(text='Да', callback_data='yes')
+item_no = types.InlineKeyboardButton(text='нет', callback_data='no')
+markup_inline.add(item_yes, item_no)
 
-bot.polling(none_stop=True, interval=0)
+@client.message_handler(commands=['get_info', 'info'])
+def get_user_info(message):
+
+    client.send_message(message.chat.id, 'Начало', reply_markup=markup_inline)
 
 
-name = '';
-surname = '';
-age = 0;
-@bot.message_handler(content_types=['text'])
-def start(message):
-    if message.text == '/reg':
-        bot.send_message(message.from_user.id, "Как тебя зовут?");
-        bot.register_next_step_handler(message, get_name); #следующий шаг – функция get_name
-    else:
-        bot.send_message(message.from_user.id, 'Напиши /reg');
+@client.callback_query_handler(func=lambda call: True)
+def answer(call):
+    if call.data == 'yes':
+        markup_reply = types.ReplyKeyboardMarkup(resize_keyboard=True , one_time_keyboard = False)
 
-def get_name(message): #получаем фамилию
-    global name;
-    name = message.text;
-    bot.send_message(message.from_user.id, 'Какая у тебя фамилия?');
-    bot.register_next_step_handler(message, get_surnme);
+        item_id = types.KeyboardButton('МОЙ ID')
+        item_username = types.KeyboardButton('МОЙ НИК')
+        item_opros = types.KeyboardButton('ОПРОС')
+
+
+        markup_reply.add(item_id, item_username,item_opros)
+        client.send_message(call.message.chat.id, 'Нажмите одну из кнопок', reply_markup=markup_reply)
+
+    elif call.data == 'no':
+        pass
+
+
+
+@client.message_handler(content_types=['text'])
+def get_text(message):
+    if message.text == 'МОЙ ID':
+        client.send_message(message.chat.id, f'Your ID: {message.from_user.id}')
+    elif message.text == 'МОЙ НИК':
+        client.send_message(message.chat.id, f'Your ID: {message.from_user.first_name}{message.from_user.last_name}')
+    elif message.text == 'ОПРОС':
+        client.send_message(message.chat.id, 'Введите ваше имя')
+        client.register_next_step_handler(message, get_surname)
 
 def get_surname(message):
-    global surname;
-    surname = message.text;
-    bot.send_message('Сколько тебе лет?');
-    bot.register_next_step_handler(message, get_age);
+    client.send_message(message.chat.id, 'Вводи сука!!!')
+    client.register_next_step_handler(message, get_suka)
 
-def get_age(message):
-    keyboard = types.InlineKeyboardMarkup(); #наша клавиатура
-    key_yes = types.InlineKeyboardButton(text='Да', callback_data='yes'); #кнопка «Да»
-    keyboard.add(key_yes); #добавляем кнопку в клавиатуру
-    key_no= types.InlineKeyboardButton(text='Нет', callback_data='no');
-    keyboard.add(key_no);
-    question = 'Тебе '+str(age)+' лет, тебя зовут '+name+' '+surname+'?';
-    bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
-@bot.callback_query_handler(func=lambda call: True)
-def callback_worker(call):
-    if call.data == "yes": #call.data это callback_data, которую мы указали при объявлении кнопки
-        #код сохранения данных, или их обработки
-        bot.send_message(call.message.chat.id, 'Запомню : )');
-    elif call.data == "no":
-        pass
-         #переспрашиваем
+def get_suka(message):
+    client.send_message(message.chat.id, 'Начало', reply_markup=markup_inline)
+
+
+
+
+
+client.polling(none_stop = True, interval = 0)
+
